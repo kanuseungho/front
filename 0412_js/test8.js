908e9e65d679d07bc0f57b20f7729158a818c6e8
kunuseungho <wnmsoso9633@naver.com>
2023-04-11 17:42:52 +0900

    20230411_1742

---
+++ 0412_js/test8.js
@@ -0,0 +1,10 @@
+function atom3(mbc){
+  document.write("hello");
+  console.log("atom3");
+  alert("atom3 입니다"+(mbc+200));
+}
+function atom4(mbc){
+  document.write("hello4");
+  console.log("atom4");
+  alert("atom4 입니다"+(mbc+200));
+}
